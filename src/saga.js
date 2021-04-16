f6fb43ded1f5cb5bef2425423f67b7c58eb80831
MRH0 <mrhmail1@gmail.com>
2021-04-16 13:45:13 +0200

    v5

--- src/saga.js
+++ src/saga.js
@@ -1,50 +1,91 @@
 /* 
-Saga Options: {
-    preventFailedBegining (true) : Throw error when calling begin() after the Saga has failed
-} 
+Saga Options:  
 */
 
+/**
+ * @class Represents a Saga transaction
+ * @author hulind KTH, arthursi KTH
+ * @argument options
+ * @description 
+ * The following are posible options:
+    - preventFailedBegining: Throw error when calling begin() after the Saga has failed (boolean).
+    - simulateFailure: Steps to fail (object where the key is a step name, and value is a boolean).
+    - debugging: Enable debugging logs (boolean).
+}
+ */
 class Saga {
-    constructor(options = {preventFailedBegining: true}) {
-        this._failed = false;
-        this._succeeded = false;
-        this._entries = [];
-        this._opts = options;
-        this._promiseResolve;
-        this._promiseReject;
-        this._promise = new Promise((resolve, reject) => {
+
+    /**
+     * @constructor instanciate a Saga transactions given optional 
+     */
+    constructor(options = {preventFailedBegining: true, simulateFailure: {}, debugging: false, reportFailedRepair: true}) {
+        /** @private */ this._failed = false;
+        /** @private */ this._succeeded = false;
+        /** @private */ this._entries = [];
+        /** @private */ this._opts = options;
+        /** @private */ this._promiseResolve;
+        /** @private */ this._promiseReject;
+        /** @private */ this._promise = new Promise((resolve, reject) => {
             this._promiseResolve = resolve;
             this._promiseReject = reject;
         })
     }
 
-    begin({name} = {}) { //Public
+    /** 
+     * @public Begin a new step
+     * @argument options
+     * @description
+     * The following are posible options:
+     * - name: provide a custom name for this step (string).
+    */ 
+    begin({name} = {}) {
         if(this._opts.preventFailedBegining && this.hasFailed())
-            throw "Can't begin(): The saga has already failed.";
+            throw "SAGA Can't begin(): The saga has already failed.";
         let se = new SagaStep(name || ("step#" + this._entries.length), this);
         this._entries.push(se);
+        if(this._opts.debugging)
+            console.log("SAGA DEBUG", "saga begin step:", step.getName());
         return se;
     }
 
-    _fail(step) { //Private
+    _fail(step) {
         this._failed = true;
-        if(step)
-            this.onStepFailed({failedStep: step});
+        if(step) {
+            this.onStepFailed({failedStep: {error: step.getError(), name: step.getName()}});
+            if(this._opts.debugging)
+                console.log("SAGA DEBUG", "step failed:", step.getName(), step.getError());
+        }
         let failedSteps = [];
         for(let entry of this._entries) {
             if(!entry.hasFailed() && !entry.hasSucceeded())
                 return;
             if(entry.hasFailed())
-                failedSteps.push(entry);
+                failedSteps.push({error: entry.getError(), name: entry.getName()});
         }
         for(let entry of this._entries)
-            if(entry.hasSucceeded())
-                entry.onRepair();
+            if(entry.hasSucceeded()) {
+                try {
+                    entry.onRepair();
+                }
+                catch(err) {
+                    entry.onFailedRepair(err);
+                    if(this._opts.reportFailedRepair)
+                        console.error("SAGA", "Error when repairing step:", entry.getName(), ":", err);
+                }
+            }
         this.onFinallyFailed({failedSteps: failedSteps});
         this._promiseReject({failedSteps: failedSteps});
+        if(this._opts.debugging)
+            console.log("SAGA DEBUG", "finally failed steps:", failedSteps);
     }
 
-    _success(name) { //Private
+    _success(step) {
+        if(this._opts.simulateFailure[step.getName()]) {
+            step.fail(this._opts.simulateFailure[step.getName()]);
+            if(this._opts.debugging)
+                console.log("SAGA DEBUG", "simulated failure on step:", step.getName());
+            return;
+        }
         if(this.hasFailed())
             return this._fail(null);
 
@@ -54,64 +95,127 @@ class Saga {
         this._succeeded = true;
         this.onFinallySucceeded({});
         this._promiseResolve({});
+        if(this._opts.debugging)
+            console.log("SAGA DEBUG", "finally succeeded");
     }
 
-    hasFailed() { //Public
+    /** 
+     * @public Has step failed
+     * @returns boolean
+    */ 
+    hasFailed() {
         return this._failed;
     }
 
-    onFinallyFailed(evt) {} //Public Event
+    /** 
+     * @public Event callback for when all steps either fail or was successful but atleat one failed.
+     * @argument event
+    */ 
+    onFinallyFailed(evt) {}
 
-    onFinallySucceeded(evt) {} //Public Event
+    /** 
+     * @public Event callback for when all steps are successful.
+     * @argument event
+    */ 
+    onFinallySucceeded(evt) {}
 
-    onStepFailed(evt) {} //Public Event
+    /** 
+     * @public Event callback for when a step fails.
+     * @argument event
+    */ 
+    onStepFailed(evt) {}
 
+    /** 
+     * @public Get the promise version of onFinallyFailed and onFinallySucceeded.
+     * @returns Promise
+    */ 
     promise() {
         return this._promise;
+    }
+
+    /** 
+     * @public Get the options set when the Saga is constructed.
+     * @returns object
+    */ 
+    getOptions() {
+        return this._opts;
     }
 }
 
 class SagaStep {
     constructor(name, saga) {
-        this._name = name;
-        this._saga = saga;
-        this._failed = false;
-        this._succeeded = false;
-        this._error = null;
+        /** @private */ this._name = name;
+        /** @private */ this._saga = saga;
+        /** @private */ this._failed = false;
+        /** @private */ this._succeeded = false;
+        /** @private */ this._error = null;
     }
 
-    success() { //Public
+    /** 
+     * @public Trigger a success in this step.
+    */ 
+    success() {
         this._succeeded = true;
-        this._saga._success(this._name);
+        this._saga._success(this);
+        if(this._saga.getOptions().debugging)
+            console.log("SAGA DEBUG", "step called success:", this.getName());
     }
 
-    fail(e = null) { //Public
+    /** 
+     * @public Trigger a failure in this step.
+     * @argument error
+    */ 
+    fail(e = null) {
         this._failed = true;
+        this._succeeded = false;
         this._error = e;
         this._saga._fail(this);
+        if(this._saga.getOptions().debugging)
+            console.log("SAGA DEBUG", "step called fail:", this.getName(), "with error: ", e);
     }
 
-    hasFailed() { //Public
+    /** 
+     * @public Has step failed
+     * @returns boolean
+    */ 
+    hasFailed() {
         return this._failed;
     }
 
-    hasSucceeded() { //Public
+    /** 
+     * @public Has step failed
+     * @returns boolean
+    */ 
+    hasSucceeded() {
         return this._succeeded;
     }
 
-    onRepair() { //Public Event
+    /** @public Event callback */ 
+    onRepair() {
     }
 
-    getError() { //Public
+    /** 
+     * @public Event callback
+     * @argument error
+    */ 
+    onFailedRepair(err) {
+    }
+
+    /** 
+     * @public Get the error provided by fail(error)
+     * @returns error
+    */ 
+    getError() {
         return this._error;
     }
 
-    getName() { //Public
+    /** 
+     * @public Get the name of the step
+     * @returns string
+    */  
+    getName() {
         return this._name;
     }
 }
-
-//SagaStep.prototype.toString = () => this._name;
-//Saga.prototype.toString = () => "[Saga]";
 
 module.exports = {Saga: Saga};
---
+++ test/test.js
@@ -0,0 +1,146 @@
+const {Saga} = require("../src/saga");
+const assert = require('assert');
+
+function waitFor(ms) {
+    return new Promise((resolve) => {
+      let id = setTimeout(() => {
+        clearTimeout(id);
+        resolve();
+      }, ms);
+    });
+}
+
+describe('Saga', function () {
+	describe('tests that saga step is created', function () {
+		let saga = new Saga();
+		it('should create a step with a name', () => {
+			let step = saga.begin({name: "test"});
+			assert.strictEqual(saga._entries[0], step);
+			assert.strictEqual(saga._entries[0].getName(), "test");
+		});
+	});
+
+	describe('tests saga with one step', () => {
+		it('should call onStepFailed', function (done) {
+			let saga = new Saga();
+			saga.onStepFailed = (e) => {
+				assert.strictEqual(e.failedStep.name, "test");
+				assert.strictEqual(e.failedStep.error, "error message");
+				done();
+			};
+
+			let step = saga.begin({name: "test"});
+			step.fail("error message");
+		});
+		
+		it('should call onFinallyFailed', (done) => {
+			let saga = new Saga();
+			saga.onFinallyFailed = (e) => {
+				assert.strictEqual(e.failedSteps.length, 1);
+				assert.strictEqual(e.failedSteps[0].name, "step#0");
+				assert.strictEqual(e.failedSteps[0].error, "error message");
+				done();
+			};
+
+			let step = saga.begin();
+			step.fail("error message");
+		});
+
+		it('should call onFinallySucceeded', (done) => {
+			let saga = new Saga();
+			saga.onFinallySucceeded = () => done();
+
+			let step = saga.begin();
+			step.success();
+		});
+	});
+
+	describe('tests saga with asynchronous promise', () => {
+		it('should complete asynchronous step', (done) => {
+			let saga = new Saga();
+
+			let step = saga.begin();
+			waitFor(10).then(() => step.success());
+
+			saga.promise().then(() => done());
+		});
+
+		it('should catch asynchronous step', (done) => {
+			let saga = new Saga();
+
+			let step = saga.begin();
+			waitFor(10).then(() => step.fail());
+
+			saga.promise().catch(() => done());
+		});
+	});
+
+	describe('tests saga with two steps', () => {
+		it('should call onStepFailed on asynchronous steps', function (done) {
+			let saga = new Saga();
+			saga.onStepFailed = (e) => {
+				assert.strictEqual(e.failedStep.name, "test2");
+				assert.strictEqual(e.failedStep.error, "error message 2");
+				done();
+			};
+
+			let step1 = saga.begin({name: "test1"});
+			let step2 = saga.begin({name: "test2"});
+
+			waitFor(10).then(() => step1.success("error message 1"));
+			waitFor(10).then(() => step2.fail("error message 2"));
+		});
+		
+		it('should call onFinallyFailed on asynchronous steps', (done) => {
+			let saga = new Saga();
+			saga.onFinallyFailed = (e) => {
+				assert.strictEqual(e.failedSteps.length, 1);
+				assert.strictEqual(e.failedSteps[0].name, "step#0");
+				assert.strictEqual(e.failedSteps[0].error, "error message 1");
+				done();
+			};
+
+			let step1 = saga.begin();
+			let step2 = saga.begin();
+
+			waitFor(10).then(() => step1.fail("error message 1"));
+			waitFor(10).then(() => step2.success("error message 2"));
+		});
+
+		it('should call onFinallySucceeded on asynchronous steps', (done) => {
+			let saga = new Saga();
+			saga.onFinallySucceeded = () => done();
+
+			let step1 = saga.begin();
+			let step2 = saga.begin();
+
+			waitFor(10).then(() => step1.success());
+			waitFor(10).then(() => step2.success());
+		});
+
+		it('should call onRepair on asynchronous step', (done) => {
+			let saga = new Saga();
+
+			let step1 = saga.begin();
+			let step2 = saga.begin();
+
+			step1.onRepair = () => done();
+
+			waitFor(10).then(() => step1.success());
+			waitFor(10).then(() => step2.fail());
+		});
+
+		it('should call onFailedRepair on asynchronous step', (done) => {
+			let saga = new Saga({reportFailedRepair: false});
+
+			let step1 = saga.begin();
+			let step2 = saga.begin();
+
+			step1.onRepair = () => { throw "error" };
+			step1.onFailedRepair = () => done();
+
+			waitFor(10).then(() => step1.success());
+			waitFor(10).then(() => step2.fail());
+		});
+	});
+});
